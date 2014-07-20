7935482417d51b33d72f751c26b58d27bf7c500a
alexanderfs <alexbupt@gmail.com>
2014-07-20 19:33:36 +0800

    change the useless file

--- FindEvents/src/com/alexan/findevents/EntranceActivity.java
+++ FindEvents/src/com/alexan/findevents/EntranceActivity.java
@@ -1,7 +1,14 @@
 package com.alexan.findevents;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class EntranceActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
 }
